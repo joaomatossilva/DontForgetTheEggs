37ef3c110b23f65cd1657ec3ea5d48ce73d180cb
Joao Matos Silva <kappy@acydburne.com.pt>
2018-04-28 23:35:53 +0100

    Finishing the validation

--- src/DontForgetTheEggs.Web/Global.asax.cs
+++ src/DontForgetTheEggs.Web/Global.asax.cs
@@ -8,6 +8,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using DontForgetTheEggs.Migrations;
 using DontForgetTheEggs.Web.Infrastructure.FeatureFolders;
+using DontForgetTheEggs.Web.Infrastructure.ModelBinders;
 
 namespace DontForgetTheEggs.Web
 {
@@ -26,6 +27,13 @@ namespace DontForgetTheEggs.Web
             //Set up the view locator for the Feature Folders layout
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new FeatureViewLocationRazorViewEngine());
+
+            //This prevents the default adding for non nullable members, because it will cause duplication
+            //with the fluentvalidation ones and it causes an error
+            DataAnnotationsModelValidatorProvider.AddImplicitRequiredAttributeForValueTypes = false;
+
+            //Model Binder to set the Model on action executing - See the class documentation for more details
+            ModelBinders.Binders.DefaultBinder = new SetModelOnViewDataModelBinder();
         }
     }
 }
---
+++ src/DontForgetTheEggs.Web/Infrastructure/ModelBinders/SetModelOnViewDataModelBinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DontForgetTheEggs.Web.Infrastructure.ModelBinders
+{
+    //This model binder set the Model on the ViewData Context in order to be able to be accessed on the OnActionExecuting action filters
+    //Taken from https://stackoverflow.com/a/25250239/4634243
+    public class SetModelOnViewDataModelBinder : DefaultModelBinder
+    {
+        protected override void OnModelUpdated(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            controllerContext.Controller.ViewData.Model = bindingContext.Model;
+            base.OnModelUpdated(controllerContext, bindingContext);
+        }
+    }
+}
