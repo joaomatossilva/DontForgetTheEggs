e476c9d1f763f315f6148e30f546763cb0e4fb14
Joao Matos Silva <kappy@acydburne.com.pt>
2018-05-16 00:32:25 +0100

    Adding adminlte by npm

---
+++ src/DontForgetTheEggs.Web/gulp.config.js
@@ -0,0 +1,43 @@
+﻿module.exports = {
+    jsFilesAfterBody: [
+        'node_modules/admin-lte/plugins/jquery/jquery.min.js',
+        'node_modules/admin-lte/plugins/bootstrap/js/bootstrap.min.js',
+        'node_modules/admin-lte/plugins/select2/select2.full.min.js',
+        'node_modules/admin-lte/plugins/datepicker/bootstrap-datepicker.js',
+        'node_modules/admin-lte/plugins/datepicker/locales/bootstrap-datepicker.pt.js',
+        'node_modules/admin-lte/plugins/timepicker/bootstrap-timepicker.js',
+        'node_modules/moment/min/moment.min.js',
+        'node_modules/moment/locale/pt.js',
+        'node_modules/admin-lte/dist/js/adminlte.min.js',
+        /*Optionally, you can add Slimscroll and FastClick plugins.
+          Both of these plugins are recommended to enhance the
+          user experience.*/
+        //'Scripts/ajax-forms.js',
+        //'Scripts/app.js'
+    ],
+    viewsToInjectScriptsAfterBody: [
+        'Features/Shared/_Layout.cshtml'
+    ],
+    viewsToInjectScriptsAfterBodyFolder: 'Features/Shared',
+
+    cssFiles: [
+        'node_modules/admin-lte/plugins/bootstrap/css/bootstrap.min.css',
+        'node_modules/admin-lte/dist/css/adminlte.min.css',
+        'node_modules/admin-lte/plugins/select2/select2.min.css',
+        'node_modules/admin-lte/plugins/datepicker/datepicker3.css',
+        'node_modules/admin-lte/plugins/timepicker/bootstrap-timepicker.min.css'
+        //'Content/css/Site.css'
+    ],
+    cssFilesRelease: [
+        'Content/css/Site.css'
+    ],
+    viewsToInjectCss: [
+        'Features/Shared/_Layout.cshtml'
+    ],
+    viewsToInjectCssFolder: 'Features/Shared',
+
+    sassMainFile: 'Features/Site.scss',
+    sassSrc: 'Features/',
+    cssDist: 'Content/css',
+    cssDistName: 'Site.css'
+}
---
+++ src/DontForgetTheEggs.Web/gulpfile.js
@@ -0,0 +1,84 @@
+﻿let gulp = require('gulp');
+let config = require('./gulp.config');
+let sass = require('gulp-sass');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let inject = require('gulp-inject');
+let print = require('gulp-print').default;
+let cleanCSS = require('gulp-clean-css');
+
+const tasks = {
+    injectJS: 'inject-js',
+    injectJSRelease: 'inject-js-release',
+    injectJSEndOfBody: 'inject-js-endofbody',
+    injectJSEndOfBodyRelease: 'inject-js-endofbody-release',
+    injectCss: 'inject-css',
+    injectCssRelease: 'inject-css-release',
+    buildSass: 'build-sass',
+    buildSassRelease: 'build-sass-release',
+    buildCssRelease: 'build-css-release'
+}
+
+gulp.task('default', [tasks.injectJS, tasks.injectCss]);
+gulp.task('Debug', ['default']); // VS task
+
+gulp.task('release', [tasks.injectJSRelease, tasks.injectCssRelease]);
+gulp.task('Release', ['release']); // VS task
+
+//TODO: there's no custom js, yet
+gulp.task(tasks.injectJS, [tasks.injectJSEndOfBody]);
+gulp.task(tasks.injectJSRelease, [tasks.injectJSEndOfBody]);
+
+gulp.task(tasks.injectJSEndOfBody, function () {
+    return gulp.src(config.viewsToInjectScriptsAfterBody)
+        .pipe(inject(gulp.src(config.jsFilesAfterBody), { starttag: '<!-- inject:EndOfBodyApp:{{ext}} -->' }))
+        .pipe(gulp.dest(config.viewsToInjectScriptsAfterBodyFolder));
+});
+
+gulp.task(tasks.injectCss/*, [tasks.buildSass]*/, function () {
+    return gulp.src(config.viewsToInjectCss)
+        .pipe(inject(gulp.src(config.cssFiles, { read: false }), { starttag: '<!-- inject:Styles:{{ext}} -->'}))
+        .pipe(gulp.dest(config.viewsToInjectCssFolder));
+});
+
+gulp.task(tasks.injectCssRelease, [tasks.buildCssRelease], function () {
+    return gulp.src(config.viewsToInjectCss)
+        .pipe(inject(gulp.src(config.cssFilesRelease), {
+            starttag: '<!-- inject:Styles:{{ext}} -->',
+            transform: function (filePath, file) {
+                return `<link rel="stylesheet" href="${filePath}?rev=${Date.now()}">`
+            }
+        }))
+        .pipe(gulp.dest(config.viewsToInjectCssFolder));
+});
+
+/*
+gulp.task(tasks.buildSass, function () {
+    return gulp.src(config.sassMainFile)
+        .pipe(sass({
+            style: 'expanded',
+            sourceComments: 'normal'
+        }).on('error', sass.logError))
+        .pipe(print())
+        .pipe(gulp.dest(config.cssDist));
+});*/
+
+gulp.task(tasks.buildSassRelease, function () {
+    return gulp.src(config.sassMainFile)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(print())
+        .pipe(gulp.dest(config.cssDist));
+});
+
+gulp.task(tasks.buildCssRelease, [tasks.buildSassRelease], function () {
+    return gulp.src(config.cssFiles)
+        .pipe(cleanCSS())
+        .pipe(concat(config.cssDistName))
+        .pipe(gulp.dest(config.cssDist));
+});
+
+gulp.task('watch', function () {
+    //Watches the sass folder for all .scss and .sass files
+    //If any file changes, run the sass task
+    gulp.watch(config.sassSrc + '**/*.scss', [tasks.buildSass]);
+});
